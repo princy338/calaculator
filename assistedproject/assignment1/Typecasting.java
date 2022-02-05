07f92dc3331415a3762b066949960dfa235692ff
Agni Zion princy Pulipati <pulipatiprincy@gmail.com>
2022-02-05 22:39:09 +0530

    typecasting

---
+++ assistedproject/assignment1/Typecasting.java
@@ -0,0 +1,31 @@
+package mainpro;
+
+public class Typecasting {
+	public static void main(String[] args) {
+		System.out.println("implicit type casting");
+		char name='a';
+		System.out.println("name is:"+name);
+		int value=name;
+		System.out.println("value is:"+value);
+		float res=name;
+		System.out.println("res is:"+res);
+		double var=name;
+		System.out.println("var is:"+var);
+		long man=name;
+		System.out.println("man is:"+man);
+		System.out.println("explict typecasting");
+		float id=98.97f;
+		int cm=(int)id;
+		long vam=(long)id;
+		System.out.println("cm is:"+cm);
+		System.out.println("vam is:"+vam);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
